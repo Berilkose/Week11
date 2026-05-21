30f9794e9d6be8c474807f2b82fccb207b167e21
BerilKose <berl.3225@gmail.com>
2026-05-21 20:42:50 +0300

    Task 8 Completed

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget { // uygulamayı şekillendirir, widgetleri 
   }
 }
 
+
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random(); // bu class uygulamanın çalışması için gereken verileri tanımlar.
   // uygulamanın asıl verisi bu class içinde tanımlanır ve bu veriler değiştiğinde uygulamanın güncellenmesi için notifyListeners() fonksiyonu çağrılır. 
@@ -42,6 +43,34 @@ class MyAppState extends ChangeNotifier {
       favorites.add(current);
     }
     notifyListeners();
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
   }
 }
 
@@ -65,7 +94,7 @@ var selectedIndex = 0;
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
