12c90b41b99d19ec9bf9d3479c3ab89dbc9382b5
BerilKose <berl.3225@gmail.com>
2026-05-23 16:03:21 +0300

    Task 2 Completed

--- Application_Part2/djangotutorial/polls/models.py
+++ Application_Part2/djangotutorial/polls/models.py
@@ -1,3 +1,25 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#Question ve Choice adında iki tane tablo modeli oluşturduk.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+    
+    #global serverdeki zaman ile karşılaştırarak, sorunun son 24 saat içinde yayınlanıp yayınlanmadığını kontrol eder.
+    #global serverlarda oluşabilecek zaman karmaşalarını çözer.
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
